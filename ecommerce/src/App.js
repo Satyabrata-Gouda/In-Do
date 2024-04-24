b09fe50ef6cc394bb56eaee289d6d8c8f6ab8eab
Libul <gouda.satyabrat@gmail.com>
2024-04-25 01:21:07 +0530

    Navigation done

---
+++ ecommerce/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/navigation/Navigation';
+import HomePage from './customer/pages/home-page/HomePage';
+
+function App() {
+  return (
+    <div className="">
+<div>
+<Navigation/>
+</div>
+<div>
+<HomePage/>
+</div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce/src/customer/pages/home-page/HomePage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import MainCarousel from '../../components/home-carousel/MainCarousel'
+
+const HomePage = () => {
+    return (
+
+        <div>
+        <MainCarousel/>
+        </div>
+    )
+}
+
+export default HomePage;
